eb34c347f1806ff9ace7a4517c999acc5ef4f100
Gabriel <gabrielja2antunes@gmail.com>
2023-02-28 19:57:58 -0300

    🚀 features: create  page Portifolio

---
+++ src/pages/Portifolio.tsx
@@ -0,0 +1,19 @@
+import Home from "../components/Home";
+import Navbar from "../components/NavBar";
+import About from "../components/About";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import Footer from "../components/Footer";
+
+export const Portifolio = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Footer />
+    </>
+  );
+};
